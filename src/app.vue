e890839170ddf1d25ec65c786a44c254106f5118
linus muema <linus.m.muema@gmail.com>
2020-04-13 12:34:27 +0300

    - Added the initial views

---
+++ src/app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+    import Uikit from 'uikit'
+    import Icons from 'uikit/dist/js/uikit-icons'
+
+    Uikit.use(Icons)
+    export default {
+
+    }
+</script>
+
+<style lang='less'>
+    @import '../node_modules/uikit/src/less/uikit.less';
+    @import '/assets/css/styles.css';
+    body,
+    #app {
+        min-height: 100vh;
+    }
+    html {
+        height: 100%;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+//views
+import index from '../views/index.vue'
+import signup from '../views/signup.vue'
+
+//components
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'index',
+    component: index
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/signup',
+    name: 'signup',
+    component: signup
   }
 ]
 
