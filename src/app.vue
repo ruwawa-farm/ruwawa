a26c914395aed3220a30a406ec86d2161d65e359
linus muema <linus.m.muema@gmail.com>
2020-04-13 13:03:50 +0300

    - Edit the index grid

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="uk-height-1-1">
     <router-view/>
   </div>
 </template>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,6 +1,14 @@
 <template>
     <div id="index">
-
+        <div class="uk-child-width-expand@s uk-text-center uk-position-center" uk-grid>
+            <div>
+                <img src="../assets/logo.png" alt="logo">
+                <h3>Welcome to Ruwawa. <br> First log into your account or create a new one.</h3>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-card-body">Item</div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,5 +19,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
