db707696b0d4b300e3c19060d0b377f18aad544a
kayere <brainhesley3@gmail.com>
2020-06-23 17:58:37 +0300

    - changed logo

--- src/components/client/signup.vue
+++ src/components/client/signup.vue
@@ -2,7 +2,7 @@
         <div id="signup">
             <div class="uk-child-width-expand@s uk-text-center uk-padding" uk-grid>
                 <div>
-                    <img src="../../assets/images/ruwawa-logo.png" alt="logo" width="50%" height="50%">
+                    <img src="../../assets/images/ruwawa-logo2.png" alt="logo" width="50%" height="50%">
                     <h3>Create an account to start using our services. <br>Are you a farmer? Join us through <router-link to="/farmer/signup">here</router-link> instead</h3>
                 </div>
                 <div>
--- src/views/index.vue
+++ src/views/index.vue
@@ -2,7 +2,7 @@
     <div id="index">
         <div class="uk-child-width-expand@s uk-text-center uk-padding" uk-grid>
             <div>
-                <img src="../assets/images/ruwawa-logo.png" alt="logo" width="50%" height="50%">
+                <img src="../assets/images/ruwawa-logo2.png" alt="logo" width="50%" height="50%">
                 <h3>Welcome to Ruwawa. <br> First log into your account or create a new one.</h3>
             </div>
             <div>
