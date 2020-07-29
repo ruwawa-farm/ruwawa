b4bd9b31d13e375a9b93ebea12290408fa7df1ce
linus muema <linus.m.muema@gmail.com>
2020-07-29 12:03:36 +0300

    - code cleanup

--- src/components/farmer/orders.vue
+++ src/components/farmer/orders.vue
@@ -5,7 +5,7 @@
         <div class="orders-list">
             <h3 class="uk-text-center uk-text-danger">{{noOrders}}</h3>
             <div uk-grid>
-                <div class="uk-card uk-card-default w3-col w3-center m2 l2 s6" v-for="(order, index) in orders" :key="order._id">
+                <div class="uk-card uk-card-default w3-col w3-center m2 l2 s6" v-for="order in orders" :key="order._id">
                     <div class="uk-card-media-top">
                         <img v-bind:src="order.product.image_url" class="profile uk-padding-small" alt="profile">
                     </div>
