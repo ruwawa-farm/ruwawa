cf0a6c08a7207affdd048d9d43d31dea5cc36adc
Peter Kayere <brainhesley3@gmail.com>
2020-12-20 09:51:16 +0300

    - Added step description for farmers

---
+++ src/components/farmer/step.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="uk-margin">
+        <div class="uk-padding-large uk-text-center">
+            <h4> {{description}} </h4>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        switch (this.step){
+            case "processing":
+                this.description = "You have received an order, please confirm after packing."
+                break;
+            case "packaged":
+                this.description = "You have succesfully packaged the order. confirm when the order is on transit."
+                break;
+            case "transit":
+                this.description = "You have succesfully submited the order to transit. Confirm when the order has arrived at the pickup location."
+                break;
+            case "delivered":
+                this.description = "The order has been completed. Please check on other orders."
+                break;
+        }
+    },
+    data() {
+        return {
+            step: this.$store.state.step,
+            description: "Processing",
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
