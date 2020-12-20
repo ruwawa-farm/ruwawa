e3b1a07d73e37625516e5102b2493da2875d950b
Peter Kayere <brainhesley3@gmail.com>
2020-12-20 09:40:37 +0300

    - Edited step description for client

--- src/components/client/step.vue
+++ src/components/client/step.vue
@@ -11,13 +11,13 @@ export default {
     mounted() {
         switch (this.step){
             case "processing":
-                this.description = "Your order is has been received and is being processed. An email will be sent to you once your order has been confirmed"
+                this.description = "Your order has been received and is being processed. An email will be sent to you once your order has been confirmed"
                 break;
             case "packaged":
                 this.description = "Your order has been packaged and ready for transportation to the pickup location"
                 break;
             case "transit":
-                this.description = "The order is on the way to the pickup location. You will the notified once it is ready for pickup"
+                this.description = "The order is on the way to the pickup location. We will notify you once it is ready for pickup"
                 break;
             case "delivered":
                 this.description = "Your order has arrived at the pickup location. An email will be sent to you with a link to the pickup location."
